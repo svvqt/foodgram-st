91f16262acba96bd54045e43c85dcc4e31f720e9
Константин Витковский <kon.vitkovskii@gmail.com>
2025-05-25 15:56:48 +0600

    добавлен workflows

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -54,6 +54,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 ROOT_URLCONF = 'foodgram.urls'
 
 TEMPLATES = [
